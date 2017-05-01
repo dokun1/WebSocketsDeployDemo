0a13bc0e29976136f2d7abcab5f176b2aba1c7df
David-Okun <david.okun@ibm.com>
2017-05-01 16:34:37 -0500

    Initial commit

---
+++ Sources/PathService.swift
@@ -0,0 +1,24 @@
+import KituraWebSocket
+import LoggerAPI
+import Foundation
+
+class PathService: WebSocketService {
+    var messageCounter = 0 
+
+    public func connected(connection: WebSocketConnection) {
+        Log.info("Connection Established: \(connection)")
+    }
+
+    public func disconnected(connection: WebSocketConnection, reason: WebSocketCloseReasonCode) {
+        Log.info("Connection closed: Reason: \(reason)")
+    }
+
+    public func received(message: Data, from: WebSocketConnection) {
+    
+    }
+ 
+    public func received(message: String, from: WebSocketConnection) {
+        messageCounter += 1
+        from.send(message: "You've sent the following message to WebSocket for path \(from.id): \"\(message)\", and this is message #\(messageCounter) for this webSocket.")
+    }
+}
---
+++ Sources/main.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Kitura
+import KituraWebSocket
+import HeliumLogger
+import LoggerAPI
+
+let router = Router()
+HeliumLogger.use()
+
+private func generateRandomString(withLength: Int) -> String {
+    Log.info("generating random string")
+    let charArray: [String] = "abcdefghijklmnopqrstuvwxyz0123456789".characters.map { String($0) }
+    var randomString = ""
+    for _ in 0 ..< withLength {
+        #if os(Linux)
+        let rand = Int(random() % charArray.count)
+        #else
+        let rand = Int(arc4random_uniform(UInt32(charArray.count)))
+        #endif
+        randomString += charArray[rand]   
+    }
+    
+    return randomString
+}
+
+router.get("/register") { request, response, next in
+    defer { next() }
+    Log.info("register path hit")
+    Log.info("sending path")
+    let path = generateRandomString(withLength: 8)
+    WebSocket.register(service: PathService(), onPath: path)
+    response.send(json: ["path" : path])   
+}
+
+let envVars = ProcessInfo.processInfo.environment
+let portString = envVars["PORT"] ?? envVars["CF_INSTANCE_PORT"] ?? envVars["VCAP_APP_PORT"] ?? "8090"
+let port = Int(portString) ?? 8090
+Kitura.addHTTPServer(onPort: port, with: router)
+Log.info("API listening on port \(port)")
+Kitura.run()
