ff242d1bf57f56affc0b106d2f90d6834d6c1c3e
David-Okun <david.okun@ibm.com>
2017-05-03 13:02:32 -0500

    Updated to use configuration package instead of raw stripping of envvars

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 8)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2)
  ]
 )
